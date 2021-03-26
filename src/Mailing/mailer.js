97eabfbb14263096feda8e94e01b76cac6abf89c
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2021-03-26 08:34:48 +0300

    Feat-Sending email using gmail OAuth2

--- src/Mailing/mailer.js
+++ src/Mailing/mailer.js
@@ -3,44 +3,60 @@ import sgMail from '@sendgrid/mail';
 import dotenv from 'dotenv';
 dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
 
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN
+    }
+  });
+
+  return transporter;
+};
 
 const sendMailWithNodeMailer = async (from,subject,message)=> {
+  try{
+    let emailTransporter = await createTransporter();
+    const emailOptions = {
+      from,
+      to: process.env.EMAIL,
+      subject,
+      html: `<p>${message}</p> <br> <p>Email sent from ${from} </p>`,
+    };
+    await emailTransporter.sendMail(emailOptions);
+  }catch(error){
+    console.log("NODE MAILER ERROR", error);
+    throw new Error(error);
+  }
 
-    return new Promise((resolve, reject) => {
-
-      const transporter = nodemailer.createTransport({
-        service:'gmail',
-        port: process.env.EMAIL_PORT,
-        secure: true,
-        auth: {
-          user: process.env.EMAIL_AUTH_USERNAME,
-          pass: process.env.EMAIL_AUTH_PASSWORD,
-        },
-      });
-
-       const mailOptions = {
-          from,
-          to: process.env.EMAIL_TO,
-          subject,
-          html: `<p>${message}</p> <br> <p>Email sent from ${from} </p>`,
-        };
-
-        // eslint-disable-next-line no-unused-vars
-        transporter.sendMail(mailOptions, (err, data) => {
-          if (err) {
-            // eslint-disable-next-line prefer-promise-reject-errors
-            reject(err);
-          }
-          resolve(true);
-        });
-      
-    }).catch(function(error) {
-      console.log("NODE MAILER ERROR", error);
-      throw new Error(error);
-    });
-  };
+}
 
 
 const sendMailWithSendGrid  = async (from,subject,message) =>{
